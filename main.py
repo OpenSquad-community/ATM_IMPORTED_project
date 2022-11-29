3448187e073179178034b43222af0e79891734ef
Akash mishra <akm008090@gmail.com>
2022-11-29 05:30:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,3 @@ while runner:
     elif n == "1":
         runner = False
 
-while runner:
-    whole.menu()  # method menu is called use 'whole' object
-    n = input(f"\n Enter '0' for 'menu' and '1' for 'exit':-")
-    if n == "0":
-
-        print(f"Thank you for using our service")
-    elif n == "1":
-        runner = False
